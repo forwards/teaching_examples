806f5bf3bcabe48f17ec1a99e55966c5773d9193
Dianne Cook <visnut@gmail.com>
2017-10-25 21:43:37 +1100

    got a map app

--- Melb_HS/pisa_world/app.R
+++ Melb_HS/pisa_world/app.R
@@ -6,45 +6,79 @@
 #
 #    http://shiny.rstudio.com/
 #
+library(tidyverse)
+library(gridExtra)
+library(plotly)
+library(ggthemes)
+library(ggmap)
+library(forcats)
+library(ISOcodes)
+data("ISO_3166_1")
+pisa_gap <- read_csv("pisa_gap.csv")
+pisa_gap <- pisa_gap %>%
+  mutate(Name = fct_reorder(Name, wmathgap)) %>%
+  mutate(direction=ifelse(wmathgap>0, "boys", "girls"))
+world_map <- map_data("world")
+
+pisa_gap <- pisa_gap %>%
+  mutate(Name = recode(Name, "Czechia"="Czech Republic",
+                       "Korea, Republic of"="South Korea",
+                       "Macedonia, Republic of"="Macedonia",
+                       "Moldova, Republic of"="Moldova",
+                       "Russian Federation"="Russia",
+                       "Taiwan, Province of China"="Taiwan",
+                       "Trinidad and Tobago"="Trinidad",
+                       "United States"="USA",
+                       "United Kingdom"="UK",
+                       "Viet Nam"="Vietnam"))
+world_map$region[world_map$subregion == "Hong Kong"] <- "Hong Kong"
+world_map$region[world_map$subregion == "Macao"] <- "Macao"
+to_map <- left_join(world_map, pisa_gap, by=c("region"="Name"))
+realvars <- c("wmath_m", "wmath_f", "wread_m", "wread_f",
+              "wscience_m", "wscience_f",
+              "wmathgap", "wreadgap", "wsciegap")
 
 library(shiny)
+library(viridis)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
+
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
+   titlePanel("PISA Scores across the globe"),
+
+   # Sidebar with a slider input for number of bins
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+        selectInput('y', 'Colour by', realvars)
       ),
-      
+
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("map")
       )
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
+
+   output$map <- renderPlot({
       # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+     ggplot(to_map, aes(map_id = region)) +
+       geom_map(aes_string(fill=input$y), map = world_map,
+                color="grey70", size=0.1) +
+       scale_fill_viridis(na.value="grey99") +
+       expand_limits(x = world_map$long, y = world_map$lat) +
+       theme_few() +
+       theme(legend.position = "bottom",
+             legend.key.width=unit(1.5, "cm"),
+             axis.ticks = element_blank(),
+             axis.title = element_blank(),
+             axis.text =  element_blank())
    })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
 
