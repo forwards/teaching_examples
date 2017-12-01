261ba1866998a910fa2f6d10153527c84ebeb27b
Dianne Cook <visnut@gmail.com>
2017-12-01 15:48:02 +1100

    outline

---
+++ NZ_women/outline.R
@@ -0,0 +1,9 @@
+# OUTLINE
+
+# 1. Explanation of package devel tools from Hadley
+# 2. Set up package structure, from scratch with RStudio (use Gretchen Albrecht palette as an example)
+# 3. Documenting functions
+# 4. CRAN checks
+# 5. pkgdown
+# 6. github repo?
+# 7. install from github
